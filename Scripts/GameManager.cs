ed43ceeda3aff16e449b41bccd0096c8797edfc9
nadjib dali ahmed <124353550+NadjibPM@users.noreply.github.com>
2023-05-25 14:47:54 +0100

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    ProgressBar pbHealth, pbEnergy, pbFood;
+    
+    //Hunger
+    [SerializeField] float decreaseFood=1f, decreaseRate=0.5f;
+
+    //Energy
+    [SerializeField] float walkEnergy = 1f, runEnergy = 3f;
+
+    bool dead = false;
+
+	void Start () {
+        StartCoroutine(DecreaseHunger());
+       
+	}
+	
+	IEnumerator DecreaseHunger()
+    {        
+        while (pbFood.Val>0)
+        {
+            pbFood.Val -= decreaseFood;
+            yield return new WaitForSeconds(decreaseRate);
+        }
+
+        GameObject.FindGameObjectWithTag("Player").GetComponent<ClaireController>().ClaireDead();
+
+    }
+
+    private void Update()
+    {
+        //Energy
+
+        if(Input.GetAxis("Vertical")!=0 && !dead)
+        {
+            pbEnergy.Val -= walkEnergy;
+
+            if (pbEnergy.Val == 0)
+            {
+                dead = true;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<ClaireController>().ClaireDead();
+            }
+
+
+        }
+
+        if (Input.GetAxis("Vertical") != 0 && Input.GetKey(KeyCode.LeftControl) && !dead)
+        {
+            pbEnergy.Val -= runEnergy;
+            if (pbEnergy.Val == 0)
+            {
+                dead = true;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<ClaireController>().ClaireDead();
+            }
+               
+        }
+    }
+    
+}
